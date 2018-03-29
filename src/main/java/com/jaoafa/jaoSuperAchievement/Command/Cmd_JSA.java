39e5e5f5474b3143791f08f9e7b5688f03710d38
Tomachi[Main] <lego@aol.jp>
2018-03-29 15:58:46 +0900

    add: jsaコマンドの追加

--- src/main/java/com/jaoafa/jaoSuperAchievement/Command/Cmd_JSA.java
+++ src/main/java/com/jaoafa/jaoSuperAchievement/Command/Cmd_JSA.java
@@ -1,9 +1,18 @@
 package com.jaoafa.jaoSuperAchievement.Command;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.jaoafa.jaoSuperAchievement.jaoSuperAchievement;
+import com.jaoafa.jaoSuperAchievement.jaoAchievement.AchievementType;
 
 public class Cmd_JSA implements CommandExecutor {
 	JavaPlugin plugin;
@@ -12,7 +21,37 @@ public class Cmd_JSA implements CommandExecutor {
 	}
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
-		return true;
+		if(!(sender instanceof Player)){
+			sender.sendMessage("[" + ChatColor.RED + "j" + ChatColor.GOLD + "a" + ChatColor.YELLOW + "o" + ChatColor.GREEN + "S" + ChatColor.AQUA + "u" + ChatColor.BLUE + "p" + ChatColor.DARK_BLUE + "e" + ChatColor.RED + "r" + ChatColor.GOLD + "A" + ChatColor.YELLOW + "c" + ChatColor.GREEN + "h" + ChatColor.AQUA + "i" + ChatColor.BLUE + "e" + ChatColor.DARK_BLUE + "v" + ChatColor.RED + "e" + ChatColor.GOLD + "m" + ChatColor.YELLOW + "e" + ChatColor.GREEN + "n" + ChatColor.AQUA + "t" + ChatColor.RESET + "] "
+					+ "このコマンドはプレイヤーからのみ実行できます。");
+			return true;
+		}
+		Player player = (Player) sender;
+		Statement statement = jaoSuperAchievement.getNewStatement();
+		Statement statement2 = jaoSuperAchievement.getNewStatement();
+		try {
+			ResultSet res_count = statement2.executeQuery("SELECT COUNT(*) FROM jaoSuperAchievement WHERE uuid = '" + player.getUniqueId().toString() + "';");
+			if(res_count.next()){
+				int i = res_count.getInt(1);
+				player.sendMessage("[" + ChatColor.RED + "j" + ChatColor.GOLD + "a" + ChatColor.YELLOW + "o" + ChatColor.GREEN + "S" + ChatColor.AQUA + "u" + ChatColor.BLUE + "p" + ChatColor.DARK_BLUE + "e" + ChatColor.RED + "r" + ChatColor.GOLD + "A" + ChatColor.YELLOW + "c" + ChatColor.GREEN + "h" + ChatColor.AQUA + "i" + ChatColor.BLUE + "e" + ChatColor.DARK_BLUE + "v" + ChatColor.RED + "e" + ChatColor.GOLD + "m" + ChatColor.YELLOW + "e" + ChatColor.GREEN + "n" + ChatColor.AQUA + "t" + ChatColor.RESET + "] "
+						+ "解除された実績が" + i + "個あります。");
+			}else{
+				player.sendMessage("[" + ChatColor.RED + "j" + ChatColor.GOLD + "a" + ChatColor.YELLOW + "o" + ChatColor.GREEN + "S" + ChatColor.AQUA + "u" + ChatColor.BLUE + "p" + ChatColor.DARK_BLUE + "e" + ChatColor.RED + "r" + ChatColor.GOLD + "A" + ChatColor.YELLOW + "c" + ChatColor.GREEN + "h" + ChatColor.AQUA + "i" + ChatColor.BLUE + "e" + ChatColor.DARK_BLUE + "v" + ChatColor.RED + "e" + ChatColor.GOLD + "m" + ChatColor.YELLOW + "e" + ChatColor.GREEN + "n" + ChatColor.AQUA + "t" + ChatColor.RESET + "] "
+						+ "解除された実績がありません。");
+				return true;
+			}
+			ResultSet res = statement.executeQuery("SELECT * FROM jaoSuperAchievement WHERE uuid = '" + player.getUniqueId().toString() + "';");
+			while(res.next()){
+				int typeid = res.getInt("achievement_typeid");
+				String date = res.getString("date");
+				AchievementType type = new AchievementType(typeid);
+				player.sendMessage("[" + ChatColor.RED + "j" + ChatColor.GOLD + "a" + ChatColor.YELLOW + "o" + ChatColor.GREEN + "S" + ChatColor.AQUA + "u" + ChatColor.BLUE + "p" + ChatColor.DARK_BLUE + "e" + ChatColor.RED + "r" + ChatColor.GOLD + "A" + ChatColor.YELLOW + "c" + ChatColor.GREEN + "h" + ChatColor.AQUA + "i" + ChatColor.BLUE + "e" + ChatColor.DARK_BLUE + "v" + ChatColor.RED + "e" + ChatColor.GOLD + "m" + ChatColor.YELLOW + "e" + ChatColor.GREEN + "n" + ChatColor.AQUA + "t" + ChatColor.RESET + "] "
+						+ type.getName() + " - " + type.getDescription() + " - " + date);
+			}
+			return true;
+		} catch (SQLException e) {
+			jaoSuperAchievement.report(e);
+			return true;
+		}
 	}
 }
