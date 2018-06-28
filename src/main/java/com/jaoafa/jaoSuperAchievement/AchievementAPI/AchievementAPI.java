d1a57fbd0346e5e28cd3e368d2ed6e5fba4c87cc
Tomachi[Main] <lego@aol.jp>
2018-06-28 15:14:14 +0900

    bugfix: java.lang.StackOverflowError

--- src/main/java/com/jaoafa/jaoSuperAchievement/AchievementAPI/AchievementAPI.java
+++ src/main/java/com/jaoafa/jaoSuperAchievement/AchievementAPI/AchievementAPI.java
@@ -1,11 +1,13 @@
 package com.jaoafa.jaoSuperAchievement.AchievementAPI;
 
+import org.bukkit.ChatColor;
+
 public class AchievementAPI {
 	/**
 	 * 「[jaoSuperAchievement] 」というプレフィックスを返します。
 	 * @return プレフィックス
 	 */
 	public static String getPrefix(){
-		return AchievementAPI.getPrefix();
+		return "[" + ChatColor.RED + "j" + ChatColor.GOLD + "a" + ChatColor.YELLOW + "o" + ChatColor.GREEN + "S" + ChatColor.AQUA + "u" + ChatColor.BLUE + "p" + ChatColor.DARK_BLUE + "e" + ChatColor.RED + "r" + ChatColor.GOLD + "A" + ChatColor.YELLOW + "c" + ChatColor.GREEN + "h" + ChatColor.AQUA + "i" + ChatColor.BLUE + "e" + ChatColor.DARK_BLUE + "v" + ChatColor.RED + "e" + ChatColor.GOLD + "m" + ChatColor.YELLOW + "e" + ChatColor.GREEN + "n" + ChatColor.AQUA + "t" + ChatColor.RESET + "] ";
 	}
 }
